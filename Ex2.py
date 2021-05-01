b182d7de7eaad7ce45aa210aa09baaaec2692cb7
danielbueeno <danielbueno@ua.pt>
2021-05-01 20:39:37 +0100

    10 exercices

---
+++ Ex2.py
@@ -0,0 +1,3 @@
+name = input("What is your name? ")
+fav_sport = input("What is your favorite sport? ")
+print(name, "likes", fav_sport)
---
+++ Ex5.py
@@ -0,0 +1,12 @@
+num1 = float(input("Insert a number: "))
+num2 = float(input("Insert other: "))
+add = num1 + num2
+sub = num1 - num2
+mult = num1 * num2
+div = num1/num2
+print("Sum:", add)
+print("Subtraction:", sub)
+print("Multiplication:", mult)
+print("Division:", div)
+
+
